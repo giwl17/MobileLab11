16021a95b7c8cfaadbc1ba4e423c9d4cf1a308b3
Giw <giw2000y@gmail.com>
2023-03-16 11:13:18 +0700

    firebase messing

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,51 @@
+// ignore_for_file: avoid_print
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+// final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+//     FlutterLocalNotificationsPlugin();
+
+// await flutterLocalNotificationsPlugin
+//   .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+//   ?.createNotificationChannel(channel);
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title
+  description:
+      'This channel is used for important notifications.', // description
+  importance: Importance.max,
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
-void main() {
+// await flutterLocalNotificationsPlugin
+//   .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+//   ?.createNotificationChannel(channel);
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -17,51 +53,35 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Notification'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
   String? message;
   String channelId = "1000";
   String channelName = "FLUTTER_NOTIFICATION_CHANNEL";
   String channelDescription = "FLUTTER_NOTIFICATION_CHANNEL_DETAIL";
+  FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
 
+  @override
   initState() {
-    message = "No message.";
-
     var initializationSettingsAndroid =
-        const AndroidInitializationSettings('notiicon');
+        AndroidInitializationSettings('notiicon');
 
     var initializationSettingsIOS = DarwinInitializationSettings(
         onDidReceiveLocalNotification: (id, title, body, payload) async {
@@ -80,82 +100,134 @@ class _MyHomePageState extends State<MyHomePage> {
       });
     });
 
+    initFirebaseMessaging();
+
     super.initState();
+  }
+
+  void initFirebaseMessaging() {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification notification = message.notification!;
+      AndroidNotification? android = message.notification?.android;
+
+      // If `onMessage` is triggered with a notification, construct our own
+      // local notification to show to users using the created channel.
+      if (android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channelDescription: channel.description,
+                icon: android.smallIcon,
+                // other properties...
+              ),
+            ));
+        //   var a = notification.title.toString();
+        //   var b = notification.body.toString();
+        //  sendNotification(a,b);
+      }
+    });
+
+    firebaseMessaging.getToken().then((String? token) {
+      assert(token != null);
+      print("Token : $token");
+    });
+  }
+
+  sendNotification(String title, String body) async {
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+      '10000',
+      'FLUTTER_NOTIFICATION_CHANNEL',
+      channelDescription: 'FLUTTER_NOTIFICATION_CHANNEL_DETAIL',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+
+    var iOSPlatformChannelSpecifics = const DarwinNotificationDetails();
+
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+        111, title, body, platformChannelSpecifics,
+        payload: 'I just haven\'t Met You Yet');
+  }
+
+  Future<void> sendPushMessage() async {
+    firebaseMessaging.getToken().then((String? _token) async {
+      assert(_token != null);
+      if (_token == null) {
+        print('Unable to send FCM message, no token exists.');
+        return;
+      }
+
+      var st = constructFCMPayload(_token);
+      try {
+        await http.post(
+          Uri.parse('https://fcm.googleapis.com/fcm/send'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Authorization':
+                'key=AAAAcHwdXtM:APA91bHydshWwd945wZs8k1RJQ9-YjA7Bdgn00KKntu506wZ8kBt9mNK_PrzeWw_XB747f04xCSM9HSKx98SEHFRjlpsK552ngtFoawKsBHLebSsJ-F5FQNyL-p3_jQwnBkuILX9mNJ9', // เปลี่ยน
+          },
+          body: st,
+        );
+        print('FCM request for device sent!');
+      } catch (e) {
+        print(e);
+      }
+      print("Token : $_token");
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        child: Text("Hello"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Your Notification App',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          sendNotification2();
+          sendPushMessage();
         },
         tooltip: 'Increment',
-        child: Icon(Icons.notifications_active),
-      ),
+        child: const Icon(Icons.notifications_active),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
 
-  sendNotification() async {
-    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
-      '10000',
-      'FLUTTER_NOTIFICATION_CHANNEL',
-      channelDescription: 'FLUTTER_NOTIFICATION_CHANNEL_DETAIL',
-      importance: Importance.max,
-      priority: Priority.high,
-    );
-
-    var iOSPlatformChannelSpecifics = const DarwinNotificationDetails();
-
-    var platformChannelSpecifics = NotificationDetails(
-        android: androidPlatformChannelSpecifics,
-        iOS: iOSPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.show(
-        111, 'สวัสดีครับ', 'ข่าวร้อนๆมาแล้ว ', platformChannelSpecifics,
-        payload: 'I just haven\'t Met You Yet');
-  }
-
-  sendNotification2() async {
-    const BigPictureStyleInformation bigPictureStyleInformation =
-        const BigPictureStyleInformation(
-      const DrawableResourceAndroidBitmap('notiicon'),
-      largeIcon: const DrawableResourceAndroidBitmap('flutter'),
-      contentTitle: 'สวัสดีครับ คุณ <b> User </b>',
-      htmlFormatContentTitle: true,
-      summaryText: 'สรุปข่าว <i> ข่าวสำคัญ </i>',
-      htmlFormatSummaryText: true,
-    );
-
-    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
-      '10000',
-      'FLUTTER_NOTIFICATION_CHANNEL',
-      channelDescription: 'FLUTTER_NOTIFICATION_CHANNEL_DETAIL',
-      importance: Importance.max,
-      priority: Priority.high,
-      styleInformation: bigPictureStyleInformation,
-    );
-
-    var iOSPlatformChannelSpecifics = const DarwinNotificationDetails();
-
-    var platformChannelSpecifics = NotificationDetails(
-        android: androidPlatformChannelSpecifics,
-        iOS: iOSPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.show(
-        111, 'สวัสดีครับ', 'ข่าวร้อนๆมาแล้ว ', platformChannelSpecifics,
-        payload: 'I just haven\'t Met You Yet');
-  }
+int _messageCount = 0;
+String constructFCMPayload(String token) {
+  _messageCount++;
+  return jsonEncode({
+    'to': token,
+    'data': {
+      'via': 'Firebase Cloud Messaging!!! ',
+      'count': _messageCount.toString(),
+    },
+    'notification': {
+      'title': 'Hello Firebase Cloud Messaging!',
+      'body': 'This notification (#$_messageCount) was created via FCM!',
+    },
+  });
 }
